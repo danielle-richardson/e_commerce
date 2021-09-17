0ff04dbd19e526b3045a38e2ef486cb567057a8c
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-16 20:18:22 -0500

    created Products component, included array of products

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Grid from '@material-ul/core';
+
+const products = [
+    {id: 1, name: 'Shoes', description: 'Running shoes.'},
+    {id: 2, name: 'Macbook', description: 'Apple macbook.'},
+];
+
+const Products = () => {
+    <main>
+        <Grid container justify="center" spacing={4}>
+            {products.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}> 
+                {/* single product components*/}
+                    <Product /> 
+                </Grid>
+            ))}
+
+        </Grid>
+    </main>
+}
+ 
+export default Products;
