1db6ace166e7007faa6e66a2f50ebd5f3b590b1b
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-17 11:15:48 -0500

    added stripe public key and payment options

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -12,7 +12,7 @@ const Products = ({ products, onAddToCart }) => {
   return (
     <main className={classes.content}>
       <div className={classes.toolbar} />
-      <Grid container justify="center" spacing={4}>
+      <Grid container justifyContent="center" spacing={4}>
         {products.map((product) => (
           <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
             <Product product={product} onAddToCart={onAddToCart} />
