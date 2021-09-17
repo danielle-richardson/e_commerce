aa2920373da83e52c64eb90d05a22834a016042d
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-17 10:06:46 -0500

    update cart buttom in navbar to link to page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import {AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core';
 import { ShoppingCart} from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
+
 import logo from '../../assets/commerce.JPG'
 
 import useStyles from './styles';
 
 const NavBar = ({totalItems}) => {
     const classes = useStyles();
+    const location = useLocation();
+
     return (
         <div>
            <AppBar position="fixed" className={classes.appBar} color="inherit">
-               <Toolbar>
-                   <Typography variant="h6" className={classes.title} color="inherit">
-                       <img src={logo} alt="E-commerce store" height="25px" className={classes.image} />
-                       E-commerce store
-                   </Typography>
-                   <div className={classes.grow} />
-                   <div className={classes.button}>
-                       <IconButton aria-label="Show cart items" color="inherit">
-                           <Badge badgeContent={totalItems} color="secondary">
-                               <ShoppingCart />
-                           </Badge>
-                       </IconButton>
-                   </div>
-               </Toolbar>
-           </AppBar>
+        <Toolbar>
+          <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+            <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> Commerce.js
+          </Typography>
+          <div className={classes.grow} />
+          {location.pathname === '/' && (
+          <div className={classes.button}>
+            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={totalItems} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+          )}
+        </Toolbar>
+      </AppBar>
         </div>
     )
 }
