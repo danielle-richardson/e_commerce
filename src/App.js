f4189936da1f629f8c00e0ccb2943a9b8e89e9d8
Danielle Ellis <thedanielleellis@gmail.com>
2021-09-16 23:31:03 -0500

    added commercejs, public key, and added fetch to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import Products from './components/Products/Products'
-import NavBar from './components/NavBar/NavBar'
+import React, {useState, useEffect} from 'react';
+import Products from './components/Products/Products';
+import NavBar from './components/NavBar/NavBar';
+import {commerce } from './lib/commerce';
 
 function App() {
+
+  // fetching products by creating a new state. 
+  const [products, setProducts] = useState([]);  
+  
+  const fetchProducts = async () => {
+    const data = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []); 
+
   return (
     <div>
         <NavBar /> 
-        <Products /> 
+        <Products products={products}/> 
     </div>
   );
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/i1-8a39d2ed-c6f8-4183-93a9-69cf4dbd0cb8/react-miler-womens-running-shoes-0mgX0d.png'},
-    {id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-gold-select-201810?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1603332211000'},
-];
+import useStyles from './styles';
 
-const Products = () => {
+const Products = ({products}) => {
+    const classes = useStyles ();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}> 
